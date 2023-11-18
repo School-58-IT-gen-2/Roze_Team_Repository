902aef603cc6766386c53e0b26c6010429b54d68
DukeTime <duke.of.times@gmail.com>
2023-11-18 20:45:54 +0300

    project

--- prothesis/Scripts/Player_class.py
+++ prothesis/Scripts/Player_class.py
@@ -1,3 +1,13 @@
 class Player():
 	def __init__(self, name):
-		pass
+		self.name = name
+		self.air = 100
+		self.health = 100
+		self.inventory = []
+		self.money = 50
+		self.protez = 100
+	def death(self, progress):
+		print(50 - progress, '[СМЕРТЬ] - В глазах темнеет... Кажется это конец...')
+		input('Новая игра - enter')
+	def inv(self):
+		print('Ваш инвентарь: ', '\n'.join(self.inventory))
