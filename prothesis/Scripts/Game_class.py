c9eb163118a63663c991d2767f5787a2cd47a50b
DukeTime <duke.of.times@gmail.com>
2023-11-18 20:40:19 +0300

    игра

--- prothesis/Scripts/Game_class.py
+++ prothesis/Scripts/Game_class.py
@@ -1,3 +1,22 @@
-class Game():
-	def __init__(self):
-		pass
+class Stage():
+
+    def __init__(self, stage_num, stage_prologue):
+        self.stage_num = stage_num
+        self.stage_prologue = stage_prologue
+        print(f'50km [НАЧАЛО] - "{self.stage_prologue}"')  #начальное сообщение
+        self.km = -1
+        self.seed = ['void'] * 50  #генерация карты (сначала заполняем все 50 мест пустыми местами)
+        self.__cycle()
+
+    def __cycle(self):
+        choice = input('выбор действия>>> ')
+        if choice == '':
+            self.step()
+
+    def step(self):
+        self.km += 1
+        eval(f'self.{self.seed[self.km]}()')
+        self.__cycle()
+
+    def void(self):
+        print(f'{50 - self.km}km [ПУСТО] - "кажется здесь пусто"')
--- prothesis/Scripts/main.py
+++ prothesis/Scripts/main.py
@@ -1,1 +1,6 @@
+from Player_class import Player
+from Game_class import Stage
 
+
+player = Player('тип')
+stage = Stage(0, 'сюжет')
