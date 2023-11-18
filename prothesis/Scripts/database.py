ef163d3c216369a07d43cc9cb643b87ad5a02434
DukeTime <duke.of.times@gmail.com>
2023-11-18 22:10:58 +0300

    database

--- prothesis/Scripts/database.py
+++ prothesis/Scripts/database.py
@@ -2,13 +2,17 @@ from NPC_class import NPC
 from Enemy_class import Enemy
 
 #БАЗА ДАННЫХ ДЛЯ ПРЕДМЕТОВ
+items = {'бинты':{'health', 25},
+         'ингалятор':{'air', 25}}
 
 
 #БАЗА ДАННЫХ ДЛЯ ОРУЖИЯ
+weapons = {'арматура':{30, 'ближнее'}}
 
 
 #БАЗА ДАННЫХ ДЛЯ НПС
 sage = NPC(name='Мудрец')
+
 
 #БАЗА ДАННЫХ ДЛЯ ВРАГОВ
 
