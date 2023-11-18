3130eaddbb7b8a73c560243c1f5df1d92cfd1b58
mshiou <147752574+mshiou@users.noreply.github.com>
2023-11-18 22:23:54 +0300

    Event_class.py

--- prothesis/Scripts/Event_class.py
+++ prothesis/Scripts/Event_class.py
@@ -22,3 +22,27 @@ class Event1(Event):
         gold_price = random.randint(50,100)
         self.money += gold_price
         print( gold_price, "Бролларов! Чтож, деньги тут ещё никому не мешали..")
+        
+class Event2(Event):
+    def __init__(self, name, description, option1, option2):
+        name='Запретный плод.'
+        description='К вам подходит робо-змея и предлагает купить Яблоко за 25 Бролларов, вы бы предпочли...'
+        super().__init__(name, description)
+        self.option1 = option1
+        self.option2 = option2
+
+    def execute(self):
+        print(self.name)
+        print(self.description)
+        print(f"Согласиться! Кто не рискует тот не пьет детское шампанское. {self.option1}")
+        print(f"Отказаться. Змей с яблоком звучит крайне опасно. {self.option2}")
+
+        choice = input("Введите номер выбранного варианта (1 или 2): ")
+
+        if choice == "1":
+            print(f"Вы решились на сделку.: {self.option1}")
+            print('А вы рисковый бро! За смелость змей вознаградил тебя мини-инголятором в виде яблока.')
+            self.air += 15
+        elif choice == "2":
+            print(f"Вы ,пожалуй, откажетесь...: {self.option2}")
+            print('Безопасность важнее риска, вы уходите, змей исчезает.')
