b330952d98d5a8f97c66362fe351411fa851b817
DukeTime <duke.of.times@gmail.com>
2023-11-30 21:57:21 +0300

    debug

---
+++ prothesis/view/player_view.py
@@ -0,0 +1,11 @@
+class PlayerView():
+    def __switch_locale(self, locale):
+        pass
+    def __init__(self, locale="RU"):
+        self.__switch_locale(locale)
+
+    def send_response_to_player(self, response):
+        pass
+
+    def get_request_from_player(self):
+        pass
