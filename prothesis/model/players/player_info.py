ee995446154fc969a5c400755ce83ca0a4e50835
DukeTime <107131066+DukeTime@users.noreply.github.com>
2023-12-18 14:45:34 +0300

    getter_and_setter1

--- prothesis/model/players/player_info.py
+++ prothesis/model/players/player_info.py
@@ -17,3 +17,29 @@ class PlayerInfo():
         print(data.keys())
         for key in list(data.keys()):
             exec(f'self.{key} = data[key]')
+
+        def set_money(self, count, player_view: PlayerView):
+        symbol = '+' if count > 0 else '-'
+        self.money = max(self.money + count, 0)
+        player_view.send_response_to_player(
+            f'{symbol} {count}k (ваш баланс: {str(self.money)} кредитов)')
+
+    def get_money(self, count=None):
+        return self.money if count is None else self.money >= count
+        #если на вход ничего не дали, то возвращаем все деньги
+        #если дали, то возвращаем больше\равны ли они указанного числа
+
+    def get_air(self, count=None):
+        return self.air if count is None else self.air >= count
+        #если на вход ничего не дали, то возвращаем весь воздух
+        #если дали, то возвращаем больше\равны ли они указанного числа
+
+    def get_health(self, count=None):
+        return self.health if count is None else self.health >= count
+        #если на вход ничего не дали, то возвращаем все хп
+        #если дали, то возвращаем больше\равны ли они указанного числа
+
+    def get_protez(self, count=None):
+        return self.protez if count is None else self.protez >= count
+        #если на вход ничего не дали, то возвращаем весь протез
+        #если дали, то возвращаем больше\равны ли они указанного числа
