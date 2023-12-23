b4a280c7a3eea927d52c368dcb70e0bc09fe9c73
Ilya <finikgrib@gmail.com>
2023-12-23 15:37:05 +0300

    database

--- prothesis/view/player_tg_view.py
+++ prothesis/view/player_tg_view.py
@@ -11,23 +11,37 @@ def start(update: Update, context: CallbackContext):
 def send_response_to_player(update: Update, context: CallbackContext, response: str):
 				update.message.reply_text(response)
 
-def get_request_from_player(update: Update, context: CallbackContext, button_func: dict):
-				custom_keyboard = [[*button_func.keys()]]
+def button(update: Update, context: CallbackContext, button_func: list):
+				custom_keyboard = button_func
 				reply_markup = ReplyKeyboardMarkup(custom_keyboard)
 				update.message.reply_text('Выберите опцию:', reply_markup=reply_markup)
 				button_click(update, context, button_func)
 
-def button_click(update: Update, context: CallbackContext):
-				text = update.message.text
-				if text in button_func:
-								button_func[text](update, context)
-				else:
-								update.message.reply_text("Пожалуйста, выберите опцию из предложенных кнопок.")
+bot = Bot(token=token)
+
+class TelegramView:
+    def __init__(self) -> None:
+        self.update_id = None
+
+    def request(self, chat_id: int = None, text: str = None):
+        if text is not None:
+            bot.send_message(chat_id, text)
+        
+        while True:
+            updates = bot.get_updates(offset=self.update_id, timeout=10000)
+            for update in updates:
+                if update.message is not None and update.message.text is not None:
+                    self.update_id = update.update_id + 1
+                    return update.message.text
+
+
+
+a = TelegramView()
+while True:
+    print(a.request())
 
 updater = Updater("6068101345:AAGr0hpElzAEBwfoc7-yoUhd-QRD9Sd8vr4")  # Замените "YOUR_BOT_TOKEN" на реальный токен вашего бота
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, button_click))
-
 updater.start_polling()
