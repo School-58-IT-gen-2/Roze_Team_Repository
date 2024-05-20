81ca2580038c31e4f02158db443220a09383e208
TimofeyYA1 <timdim2018@mail.ru>
2024-05-20 20:36:41 +0300

    clear code

--- prothesis/view/player_tg_view.py
+++ prothesis/view/player_tg_view.py
@@ -6,13 +6,15 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import Update
 from telegram.ext import CallbackContext
 from typing import List
-
-
+from dotenv import load_dotenv
+import os
 
 
 #ПРИМЕР МИШИ
-token = "6712575033:AAFi3-Juz0w3dlOSBNU4AAZDtYxwOAqrRTA"
-bot = Bot(token=token)
+load_dotenv()
+tg_token = os.getenv('token')
+
+bot = Bot(token=tg_token)
 
 
 class PlayerTGView(PlayerView):
@@ -66,70 +68,3 @@ class PlayerTGView(PlayerView):
     
     def way_report(self, km, place, text):
         bot.send_message(self.chat_id, f'{61 - km}km [{place}] - {text}')
-
-
-'''class PlayerTGView():
-    def __init__(self, locale="RU"):
-        super().__init__(locale)
-
-    def send_response_to_player(self, response):
-        bot.send_message(chat_id, response)
-
-    def get_request_from_player(self, chat_id: int = None, text: str = None):
-        if text is not None:
-            bot.send_message(chat_id, text)
-        
-        while True:
-            updates = bot.get_updates(offset=self.update_id, timeout=10000)
-            for update in updates:
-                if update.message is not None and update.message.text is not None:
-                    self.update_id = update.update_id + 1
-                    return update.message.text
-    
-    def way_report(self, km, place, text):
-        bot.send_message(chat_id, f'{61 - km}km [{place}] - {text}')'''
-
-#СТАРЫЙ КОД
-'''
-
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("привет")
-
-def send_response_to_player(update: Update, context: CallbackContext, response: str):
-				update.message.reply_text(response)
-
-def button(update: Update, context: CallbackContext, button_func: list):
-				custom_keyboard = button_func
-				reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-				update.message.reply_text('Выберите опцию:', reply_markup=reply_markup)
-				button_click(update, context, button_func)
-
-bot = Bot(token=token)
-
-class TelegramView:
-    def __init__(self) -> None:
-        self.update_id = None
-
-    def request(self, chat_id: int = None, text: str = None):
-        if text is not None:
-            bot.send_message(chat_id, text)
-        
-        while True:
-            updates = bot.get_updates(offset=self.update_id, timeout=10000)
-            for update in updates:
-                if update.message is not None and update.message.text is not None:
-                    self.update_id = update.update_id + 1
-                    return update.message.text
-
-
-
-a = TelegramView()
-while True:
-    print(a.request())
-
-updater = Updater("6068101345:AAGr0hpElzAEBwfoc7-yoUhd-QRD9Sd8vr4")  # Замените "YOUR_BOT_TOKEN" на реальный токен вашего бота
-dispatcher = updater.dispatcher
-
-dispatcher.add_handler(CommandHandler("start", start))
-updater.start_polling()
-'''
